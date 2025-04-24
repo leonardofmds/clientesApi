f855019cc5094cebe2b6320ed3d7324e1beda106
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-23 22:08:08 -0300

    Client POST

--- src/main/java/leonardofmds/clientesApi/controllers/ClientesController.java
+++ src/main/java/leonardofmds/clientesApi/controllers/ClientesController.java
@@ -1,15 +1,22 @@
 package leonardofmds.clientesApi.controllers;
 
+import jakarta.validation.Valid;
+import leonardofmds.clientesApi.dtos.ClienteRequestDto;
+import leonardofmds.clientesApi.dtos.ClienteResponseDto;
+import leonardofmds.clientesApi.services.impl.ClienteServiceImpl;
+import leonardofmds.clientesApi.services.interfaces.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/clientes")
 public class ClientesController {
 
+    @Autowired ClienteService clienteService;
 
     @PostMapping
-    public String criarCliente() {
-        return "Cliente criado com sucesso!";
+    public ClienteResponseDto criarCliente(@RequestBody @Valid ClienteRequestDto request) throws Exception {
+        return clienteService.cadastrar(request);
     }
     @PutMapping("/{id}")
     public String atualizarCliente() {
--- src/main/java/leonardofmds/clientesApi/services/impl/ClienteServiceImpl.java
+++ src/main/java/leonardofmds/clientesApi/services/impl/ClienteServiceImpl.java
@@ -2,11 +2,16 @@ package leonardofmds.clientesApi.services.impl;
 
 import leonardofmds.clientesApi.dtos.ClienteRequestDto;
 import leonardofmds.clientesApi.dtos.ClienteResponseDto;
+import leonardofmds.clientesApi.entities.Cliente;
+import leonardofmds.clientesApi.entities.Endereco;
 import leonardofmds.clientesApi.repositories.ClienteRepository;
 import leonardofmds.clientesApi.services.interfaces.ClienteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class ClienteServiceImpl implements ClienteService {
@@ -16,7 +21,22 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Override
     public ClienteResponseDto cadastrar(ClienteRequestDto request) throws Exception {
-        return null;
+
+        Cliente cliente = new Cliente();
+        Endereco endereco = new Endereco();
+        BeanUtils.copyProperties(request.getEndereco(),endereco);
+        BeanUtils.copyProperties(request,cliente);
+        cliente.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").parse(request.getDataNascimento())); //capturando o campo data;
+        cliente.setEnderecos(new ArrayList<Endereco>());
+        cliente.getEnderecos().add(endereco);
+
+        clienteRepository.save(cliente);
+
+        ClienteResponseDto response = new ClienteResponseDto();
+        BeanUtils.copyProperties(cliente,response);
+        response.setDataNascimento(new SimpleDateFormat("dd/MM/yyyy").format(cliente.getDataNascimento()));
+
+        return response;
     }
 
     @Override
